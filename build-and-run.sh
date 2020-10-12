68bd912e0af0a1afc29d2e19e2705127bd87cd5e
ronek22 <jronek3010@gmail.com>
2020-10-12 17:10:28 +0200

    Fix createsuperuser in build-and-run

--- build-and-run.sh
+++ build-and-run.sh
@@ -7,5 +7,5 @@ EXEC_CMD='docker-compose run backend'
 docker-compose up --build -d
 
 ${EXEC_CMD} python manage.py migrate
-${EXEC_CMD} python manage.py createsuperuser --no-input
+${EXEC_CMD} python manage.py createsuperuser --username test1 --password 123321 --no-input
 ${EXEC_CMD} python manage.py test
