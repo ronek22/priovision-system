0619ad2a6fbdee888ad405082bef4b96659d851d
ronek22 <jronek3010@gmail.com>
2020-10-24 18:39:38 +0200

    Fix tests after made changes in implementation

--- backend/upc/tests.py
+++ backend/upc/tests.py
@@ -48,51 +48,51 @@ class UpcClientTestCase(APITestCase):
 
     def testProfitFromNewClients(self):
         Client.objects.create(created_by=self.user, number=1, total=50.0)
-        self.assertEqual(calculate_new_clients(Client.objects), 25.0)
+        self.assertEqual(calculate_new_clients(Client.objects)[0], 25.0)
 
         Client.objects.create(created_by=self.user, number=2, total=100.0)
-        self.assertEqual(calculate_new_clients(Client.objects), 145.0)
+        self.assertEqual(calculate_new_clients(Client.objects)[0], 145.0)
 
         # Add 10 clients with 75 PLN TOTAL, AMOUNT OF CLIENTS: 12
         # First client with amount of 50, now become worth 40 instead of 25
         Client.objects.bulk_create([Client(created_by=self.user,number=x,total=80.0) for x in range(3,13)])
-        self.assertEqual(calculate_new_clients(Client.objects), 960.0)
+        self.assertEqual(calculate_new_clients(Client.objects)[0], 960.0)
 
     def testProfitFromOldClientsWithoutPremium(self):
         obj = Client.objects.create(created_by=self.user, number=1, total=25.00, core=15.00) # total - 1 prog, 
-        self.assertEqual(calculate_old_clients(Client.objects), float(obj.total)*1.7)
+        self.assertEqual(calculate_old_clients(Client.objects)[0], float(obj.total)*1.7)
 
         Client.objects.filter(pk=obj.pk).update(core=21.00)
         obj.refresh_from_db()
-        self.assertEqual(calculate_old_clients(Client.objects), float(obj.total)*1.8)
+        self.assertEqual(calculate_old_clients(Client.objects)[0], float(obj.total)*1.8)
 
         Client.objects.filter(pk=obj.pk).update(core=5.00)
         obj.refresh_from_db()
-        self.assertEqual(calculate_old_clients(Client.objects), float(obj.total)*1.5+3)
+        self.assertEqual(calculate_old_clients(Client.objects)[0], float(obj.total)*1.5+3)
 
         Client.objects.filter(pk=obj.pk).update(total=30.00)
         obj.refresh_from_db()
-        self.assertEqual(calculate_old_clients(Client.objects), float(obj.total)*1.7+3)
+        self.assertEqual(calculate_old_clients(Client.objects)[0], float(obj.total)*1.7+3)
 
         Client.objects.filter(pk=obj.pk).update(core=15.00)
         obj.refresh_from_db()
-        self.assertEqual(calculate_old_clients(Client.objects), float(obj.total)*1.8)
+        self.assertEqual(calculate_old_clients(Client.objects)[0], float(obj.total)*1.8)
 
         Client.objects.filter(pk=obj.pk).update(core=21.00)
         obj.refresh_from_db()
-        self.assertEqual(calculate_old_clients(Client.objects), float(obj.total)*2.0)
+        self.assertEqual(calculate_old_clients(Client.objects)[0], float(obj.total)*2.0)
 
     def testProfitFromOldClientWithPremium(self):
         obj = Client.objects.create(created_by=self.user, number=1, total=25.00, core=15.00, premium=10.00)
-        self.assertEqual(calculate_old_clients(Client.objects), float(obj.total)*1.7+10.00)
+        self.assertEqual(calculate_old_clients(Client.objects)[0], float(obj.total)*1.7+10.00)
 
         Client.objects.filter(pk=obj.pk).update(premium=15.00)
         obj.refresh_from_db()
-        self.assertEqual(calculate_old_clients(Client.objects), float(obj.total)*1.7+15.0*1.4)
+        self.assertEqual(calculate_old_clients(Client.objects)[0], float(obj.total)*1.7+15.0*1.4)
 
         Client.objects.filter(pk=obj.pk).update(premium=30.00)
         obj.refresh_from_db()
-        self.assertEqual(calculate_old_clients(Client.objects), float(obj.total)*1.7+30.0*1.8)
+        self.assertEqual(calculate_old_clients(Client.objects)[0], float(obj.total)*1.7+30.0*1.8)
 
         
 
